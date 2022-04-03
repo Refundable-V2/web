ef4a6328fe39472c095db9ecc54153e77afc9ebf
mstubenvoll-tgm <31843666+mstubenvoll@users.noreply.github.com>
2022-04-04 00:22:58 +0200

    Add files via upload
    Navbar geadded

--- src/components/admin/AdminDashboard.vue
+++ src/components/admin/AdminDashboard.vue
@@ -1,20 +1,33 @@
 <!-- Admin Dashboard Template -->
 <template>
   <b-container fluid>
+    <!-- NAVBAR-->
+<nav class="navbar navbar-expand-lg py-3 navbar-dark bg-dark shadow-sm">
+  <div class="container">
+    <a href v-on:click="index" class="navbar-brand">
+      <!-- Logo Image -->
+      <img src="@/assets/logo.svg" width="45" alt="" class="d-inline-block align-middle mr-2">
+      <!-- Logo Text -->
+      <span class="text-uppercase font-weight-bold">Refundable</span>
+    </a>
+
+    <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+
+    <div id="navbarSupportedContent" class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a href v-on:click="normal" class="nav-link">Home </a></li>
+        <li class="nav-item"><a href v-on:click="accountView" class="nav-link">Konto </a></li>
+        <li class="nav-item"><a href v-on:click="logout" class="nav-link">Abmelden</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <!-- User Interface controls -->
     <b-row align-v="center" align-h="center">
       <b-col cols="12" md="6">
-        <h1 id="new-application-heading">Alle Anträge</h1>
+        <h1 id="new-application-heading">Alle Anträge (Admin)</h1>
       </b-col>
       <div class="col-12 col-md-6">
-        <!-- Logout Button -->
-        <b-button
-          variant="outline-danger"
-          class="float-right"
-          v-on:click="logout"
-        >
-          Abmelden
-        </b-button>
         <!-- Ansicht wechseln Button -->
         <b-button
           variant="outline-primary"
@@ -1004,6 +1017,14 @@ export default {
         appendToast: false,
         variant: "danger"
       });
+    },
+    /**
+     * Diese Methode leitet den Benutzer auf die AccountView-Seite weiter
+     */
+    accountView() {
+      if (this.checkClick()) {
+        this.changeComponent("AccountView");
+      }
     },
     /**
      * Diese Methode zeigt dem Benutzer an, dass der Antrag erfolgreich gespeichert worden ist
