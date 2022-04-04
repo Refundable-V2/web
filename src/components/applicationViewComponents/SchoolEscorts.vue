93f2a86d1ae6f6d24935e84b3f0b62738c87349e
mstubenvoll-tgm <31843666+mstubenvoll@users.noreply.github.com>
2022-04-04 02:09:49 +0200

    Add files via upload
    Navbar geadded

--- src/components/applicationViewComponents/SchoolEscorts.vue
+++ src/components/applicationViewComponents/SchoolEscorts.vue
@@ -1,6 +1,27 @@
 <!-- Template fpr die Anträge der Begleiter der Schulveranstaltungen -->
 <template>
   <b-container fluid>
+    <!-- NAVBAR-->
+<nav class="navbar navbar-expand-lg py-3 navbar-dark bg-dark shadow-sm">
+  <div class="container">
+    <a href v-on:click="index" class="navbar-brand">
+      <!-- Logo Image -->
+      <img src="@/assets/logo.svg" width="45" alt="" class="d-inline-block align-middle mr-2">
+      <!-- Logo Text -->
+      <span class="text-uppercase font-weight-bold">Refundable</span>
+    </a>
+
+    <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+
+    <div id="navbarSupportedContent" class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a href v-on:click="index" class="nav-link">Home </a></li>
+        <li class="nav-item"><a href v-on:click="accountView" class="nav-link">Konto </a></li>
+        <li class="nav-item"><a href v-on:click="logout" class="nav-link">Abmelden</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <b-row>
       <b-col cols="12">
         <!-- Input für das Startdatum der SV -->
@@ -183,6 +204,22 @@ export default {
       this.updateData();
     }
   },
+  /**
+     * Diese Methode meldet den Benutzer vom System ab
+     */
+    logout() {
+      if (this.checkClick()) {
+        this.$emit("logout");
+      }
+    },
+    /**
+     * Diese Methode leitet den Benutzer auf die AccountView-Seite weiter
+     */
+    accountView() {
+      if (this.checkClick()) {
+        this.changeComponent("AccountView");
+      }
+    },
   mounted() {
     this.selected = this.data.group;
     var start = new Date(this.data.attendance_from);
--- src/components/applicationViewComponents/Workshop.vue
+++ src/components/applicationViewComponents/Workshop.vue
@@ -1,6 +1,27 @@
 <!-- Template für Fortbildungsanträge -->
 <template>
   <b-container fluid>
+    <!-- NAVBAR-->
+<nav class="navbar navbar-expand-lg py-3 navbar-dark bg-dark shadow-sm">
+  <div class="container">
+    <a href v-on:click="index" class="navbar-brand">
+      <!-- Logo Image -->
+      <img src="@/assets/logo.svg" width="45" alt="" class="d-inline-block align-middle mr-2">
+      <!-- Logo Text -->
+      <span class="text-uppercase font-weight-bold">Refundable</span>
+    </a>
+
+    <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+
+    <div id="navbarSupportedContent" class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a href v-on:click="index" class="nav-link">Home </a></li>
+        <li class="nav-item"><a href v-on:click="accountView" class="nav-link">Konto </a></li>
+        <li class="nav-item"><a href v-on:click="logout" class="nav-link">Abmelden</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <b-row align-h="center">
       <b-col cols="12">
         <b-container fluid>
@@ -232,6 +253,22 @@ export default {
       endTime: ""
     };
   },
+  /**
+     * Diese Methode meldet den Benutzer vom System ab
+     */
+    logout() {
+      if (this.checkClick()) {
+        this.$emit("logout");
+      }
+    },
+    /**
+     * Diese Methode leitet den Benutzer auf die AccountView-Seite weiter
+     */
+    accountView() {
+      if (this.checkClick()) {
+        this.changeComponent("AccountView");
+      }
+    },
   mounted() {
     var start = new Date(this.data.start_time);
     var end = new Date(this.data.end_time);
